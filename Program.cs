e152a260cf7ed9a89e90116edd178151ed05e2a9
Apowalowski <acpowalowski@gmail.com>
2021-04-05 15:38:42 -0400

    methodexercise

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Methodexercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What is your dog's name?");
+            string userDogName = Console.ReadLine();
+
+            Console.WriteLine("What is your dog's color of fur?");
+            string dogColor = Console.ReadLine();
+
+            Console.WriteLine($"{userName} is {age}.");
+            Console.WriteLine($"{userDogName} is his dog. He is a good boy.");
+            Console.WriteLine($"His dog's fur is {dogColor}");
+
+
+
+        }
+    }
+}
