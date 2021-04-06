bd706afab80c803a92c09365d8d8a1f480ff0b9c
Apowalowski <acpowalowski@gmail.com>
2021-04-05 20:45:08 -0400

    Methodexercise2

--- Program.cs
+++ Program.cs
@@ -6,24 +6,29 @@ namespace Methodexercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your name?");
-            string userName = Console.ReadLine();
+        
 
-            Console.WriteLine("What is your age?");
-            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Give me a number to add.");
+            int num1 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("What is your dog's name?");
-            string userDogName = Console.ReadLine();
+            Console.WriteLine("Give me a number to add to the first one.");
+            int num2 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("What is your dog's color of fur?");
-            string dogColor = Console.ReadLine();
-
-            Console.WriteLine($"{userName} is {age}.");
-            Console.WriteLine($"{userDogName} is his dog. He is a good boy.");
-            Console.WriteLine($"His dog's fur is {dogColor}");
+            int sum = Sum(num1, num2);
+            Console.WriteLine($"The sum is {sum}.");
 
 
+        }
+        public static int Sum(int num1, int num2)
+        {
+            //int sum = num1 + num2;
+            //return sum; 
 
+            return num1 + num2;
+        }
+        public static int Multiply(int x, int y)
+        {
+            return x * y; 
         }
     }
 }
